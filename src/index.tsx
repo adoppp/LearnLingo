01b59adcf4ca97afde4da43a350a209f9c9307f5
Adop <chdn786od@gmail.com>
2023-12-15 10:46:26 +0100

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App'; 
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react_vite_ts-template">
+    <BrowserRouter basename="/LearnLingo">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
